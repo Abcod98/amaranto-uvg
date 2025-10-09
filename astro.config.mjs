f952ac48637fdd1b62d8d64ab2b76c93ab0bad0a
Abraham  Tun <abrahamtun@MacBook-Pro-de-Abraham.local>
2025-10-08 23:56:35 -0600

    Navegación, Inicio, Tienda

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://amaranto-guatemala.com',
+  compressHTML: true,
+  build: {
+    assets: '_assets'
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/styles/variables.scss";`
+        }
+      }
+    }
+  }
+});
